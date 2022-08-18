29cf431c41a49981a25ad0a08c6bc87aee5635ca
Cesar Varettoni <cvarettoni@gmail.com>
2022-08-17 21:51:42 -0300

    Confección NavBar - Desafío 2 - ReactJS

--- src/index.js
+++ src/index.js
@@ -15,3 +15,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+let data = [1,2,3];
+let result= data.find(element => element>1);
+console.log (element);
+
