5536320c9c8eaf68251ccc5da9263719c69a5c0d
Cesar Varettoni <cvarettoni@gmail.com>
2022-08-17 22:04:03 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className='navbar'>
+      <h2>MENDO-SPORT</h2>
+        <ul className='navbar__lista'>
+          <li><a className='navbar__lista__cat' href='#'>Balones</a></li>
+          <li><a className='navbar__lista__cat' href='#'>Calzado</a></li>
+          <li><a className='navbar__lista__cat' href='#'>Accesorios</a></li>
+        </ul>
+      <button className='navbar__boton'>REGISTER</button>
+    </header>
+
   );
 }
 
